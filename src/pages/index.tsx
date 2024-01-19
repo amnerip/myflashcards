f1114001be08a94301c661a176b766f85229af61
Ana M. Neri <neriam@mit.edu>
2024-01-18 23:25:56 -0500

    fix build issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 import { log } from "next-axiom";
@@ -19,7 +18,7 @@ export default function Home() {
       <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            MyFlashcards - Ana's app!
+            MyFlashcards - Ana&apos;s app!
           </h1>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
