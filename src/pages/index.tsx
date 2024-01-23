1879f6be3c4ae4797288cb603254a53ad3c6b0b1
Ana M. Neri <neriam@mit.edu>
2024-01-22 19:50:40 -0500

    Clean up home page, introduce new components to improve readability

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const { data: decks } = api.deck.getAllDecks.useQuery();
+  // TODO: check for query completion, render some loading state until completion.
+  if (!decks) return <div/>
 
   return (
     <>
@@ -15,22 +17,30 @@ export default function Home() {
       </Head>
 
       <main className="flex justify-center">
-        <div className="">
+        <div>
           <Header />
-
-          <div className="flex flex-row md:max-w-2xl gap-4 px-8 py-4">
-            {decks?.map((deck) => (
-              <div key={deck.id} className="px-28 py-14 rounded-xl bg-slate-300 hover:bg-slate-200">{deck.name}</div>
-            ))}
-          </div>
+          <DecksList items={decks.map((d) => ({id: d.id, name: d.name}))} />
         </div>
       </main>
     </>
   );
 }
 
+function DecksList(props: { items: { id: number, name: string }[] }) {
+  return (
+    <div className="flex flex-row md:max-w-2xl gap-4 px-8 py-4">
+      {props.items.map(
+        (deck) => (
+          <div key={deck.id} className="px-28 py-14 rounded-xl bg-slate-300 hover:bg-slate-200">{deck.name}</div>
+        )
+      )}
+    </div>
+  )
+}
+
 function Header() {
   const { data: sessionData } = useSession();
+  // TODO: check for query completion, render some loading state until completion.
   return <>
     <div className="flex flex-row justify-end items-right w-screen bg-blue-50 p-4">
       <div className="px-6 py-2">
@@ -43,7 +53,7 @@ function Header() {
 
 function AuthShowcase() {
   const { data: sessionData } = useSession();
-
+  // TODO: check for query completion, render some loading state until completion.
   return (
     <div className="">
       <button
